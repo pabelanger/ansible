8b0ba58a10cdf98b765fba04a4fd1fcd4b8e0512
Paul Belanger <pabelanger@redhat.com>
2019-10-03 16:25:23 -0400

    WIP: Do not merge, testing python38 jobs for network
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/217
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- lib/ansible/modules/network/vyos/vyos_banner.py
+++ lib/ansible/modules/network/vyos/vyos_banner.py
@@ -23,6 +23,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'network'}
 
+
 DOCUMENTATION = """
 ---
 module: vyos_banner
